69afb175e516472f61c76e381fed999e48a13674
Johandre Fouche <fouchejohandre@gmail.com>
2019-01-08 17:39:40 +0200

    Added ZERO Functions - Test freeCodeCamp

--- src/Components/address-login/address-login.component.js
+++ src/Components/address-login/address-login.component.js
@@ -7,25 +7,64 @@ angular.
     controller: ['$state', '$http', '$scope', 
       function AddressHeaderLoginController($state, $http, $scope) {
       var self = this;
-      self.str1 = 'A,C.D!';
-      self.str2 = '';
-      self.chars = ['!','@','#','$','%','^','&','*','(',')','-','_','=','+','{','[',']','}',':',';','|','/','?','.','>',',','<','0','1','2','3','4','5','6','7','8','9','`', ' ']
-      self.strChars = [];
+      // Project Euler: Problem 1: Multiples of 3 and 5
+      //   self.number = 1000;
+      //   self.array = [];
+      //   self.result = 0;
+      //   for(var i = 0; i < self.number; i++)
+      //   {
+      //     if(i % 3 === 0 || i % 5 === 0)
+      //     {
+      //       self.array.push(i);
+      //     }
+      //   }
+      // console.log(self.array);
+      //   for (var x = 0; x < self.array.length; x++)
+      //   {
+      //     if(self.result === 0)
+      //     {
+      //       self.result = self.array[x];
+      //     }
+      //     else
+      //     {
+      //       self.result = self.result + self.array[x];
+      //     }
+      //   }
+      //   console.log(self.result);
+
+
+      function canMakeWord (word) {
+        var blocks = ['B', 'O', 'X', 'K', 'D', 'Q', 'C', 'P', 'N', 'A', 'G', 'T', 'R', 'E', 'T', 'G', 'Q', 'D', 'F', 'S', 'J', 'W', 'H', 'U', 'V', 'I', 'A', 'N', 'O', 'B', 'E', 'R', 'F', 'S', 'L', 'Y', 'P', 'C', 'Z', 'M'];
+      
+        var wordArray = word.split('');
+      
+        var can;
+      
+        for(var i = 0; i < blocks.length; i++)
+        {
+          for(var x = 0; x < wordArray.length; x++)
+          {
+            if(wordArray[x] == blocks[i])
+            {
+              wordArray.splice(x, 1);
+            }
+          }
+        }
+      
+        if(wordArray.length > 0)
+        {
+          can = true;
+        }
+        else
+        {
+          can = false;
+        }
+        console.log(can);
+
+
       self.$onInit = function(){
         $state.transitionTo("home");
-        for(var i=0; i< self.chars.length; i++)
-        {
-          self.str1 = self.str1.replace(self.chars[i], '');
-        }
-        self.strChars = self.str1.split('');
-        self.strChars = self.strChars.reverse();
-        self.str2 = self.strChars.join();
-        for(var i=0; i< self.chars.length; i++)
-        {
-          self.str2 = self.str2.replace(self.chars[i], '');
-        }
-        console.log(self.str1);
-        console.log(self.str2);
+        
       }
 
       }]
